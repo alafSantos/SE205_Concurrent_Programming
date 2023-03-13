37864a79f139cfec8a03dbdc469386acf627d223
Alaf Do Nascimento Santos <asantos-22@tp-4b01-31.enst.fr>
2023-03-13 10:54:57 +0100

    1.1 Process creation and termination in order of creation

--- TP04/src/BoundedBufferMain.java
+++ TP04/src/BoundedBufferMain.java
@@ -1,18 +1,18 @@
 public class BoundedBufferMain {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         BoundedBuffer buffer;
 
         // Check the arguments of the command line
-        if (args.length != 1){
-            System.out.println ("PROGRAM FILENAME");
+        if (args.length != 1) {
+            System.out.println("PROGRAM FILENAME");
             System.exit(1);
         }
         Utils.init(args[0]);
 
-        Consumer consumers[] = new Consumer[(int)Utils.nConsumers];
-        Producer producers[] = new Producer[(int)Utils.nProducers];
-        
+        Consumer consumers[] = new Consumer[(int) Utils.nConsumers];
+        Producer producers[] = new Producer[(int) Utils.nProducers];
+
         // Create a buffer
         if (Utils.sem_impl == 0)
             buffer = new NatBoundedBuffer(Utils.bufferSize);
@@ -21,7 +21,13 @@ public class BoundedBufferMain {
 
         // Create producers and then consumers
         // Create consumers
+        for (int i = 0; i < (int) Utils.nConsumers; i++) {
+            new Thread(consumers[i]).start();
+        }
 
-// Create producers
+        // Create producers
+        for (int i = 0; i < (int) Utils.nProducers; i++) {
+            new Thread(producers[i]).start();
+        }
     }
 }
