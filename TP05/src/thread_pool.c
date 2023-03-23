a1802502d8c7917e21d3cef602090924a3a2afda
Alaf Do Nascimento Santos <asantos-22@tp-3b07-09.enst.fr>
2023-03-23 15:17:05 +0100

    1.2 Implementation of a basic threads manager

--- TP05/src/thread_pool.c
+++ TP05/src/thread_pool.c
@@ -9,7 +9,8 @@ int pt_debug = 0;
 
 // Create a thread pool. This pool must be protected against
 // concurrent accesses.
-thread_pool_t *thread_pool_init(int core_pool_size, int max_pool_size) {
+thread_pool_t *thread_pool_init(int core_pool_size, int max_pool_size)
+{
   thread_pool_t *thread_pool;
 
   thread_pool = (thread_pool_t *)malloc(sizeof(thread_pool_t));
@@ -17,6 +18,7 @@ thread_pool_t *thread_pool_init(int core_pool_size, int max_pool_size) {
   thread_pool->core_pool_size = core_pool_size;
   thread_pool->max_pool_size = max_pool_size;
   thread_pool->size = 0;
+  pthread_mutex_init(&thread_pool->m, NULL);
   return thread_pool;
 }
 
@@ -27,19 +29,24 @@ thread_pool_t *thread_pool_init(int core_pool_size, int max_pool_size) {
 // thread status (NOTHREAD, PERMANENT, TEMPORARY)
 
 int pool_thread_create(thread_pool_t *thread_pool, main_func_t main,
-                       void *main_params, int force) {
+                       void *main_params, int force)
+{
   pool_thread_main_params *extended_main_params;
   int status = NOTHREAD;
   pthread_t thread;
   char *task_name;
 
-  if (thread_pool->shutdown) return 0;
+  if (thread_pool->shutdown)
+    return 0;
 
   /*
     Protect structure against concurrent accesses
   */
 
-  if (thread_pool->size < thread_pool->core_pool_size) {
+  pthread_mutex_lock(&thread_pool->m);
+
+  if (thread_pool->size < thread_pool->core_pool_size)
+  {
     // Always create a thread as long as there are less then
     // core_pool_size threads created.
 
@@ -52,10 +59,18 @@ int pool_thread_create(thread_pool_t *thread_pool, main_func_t main,
     extended_main_params->id = thread_pool->size;
 
     // Create a thread to execute the main function passed as parameters
+    if (pthread_create(&thread, NULL, main, extended_main_params) != 0)
+    {
+      thread_pool->size--;
+      perror("pthread_create() error\n");
+      exit(1);
+    }
 
     asprintf(&task_name, "core %02d", extended_main_params->id);
     set_task_name(extended_main_params->id, task_name);
-  } else if ((force) && (thread_pool->size < thread_pool->max_pool_size)) {
+  }
+  else if ((force) && (thread_pool->size < thread_pool->max_pool_size))
+  {
     status = TEMPTHREAD;
     thread_pool->size++;
     extended_main_params =
@@ -68,20 +83,23 @@ int pool_thread_create(thread_pool_t *thread_pool, main_func_t main,
   }
 
   // Do not protect the structure against concurrent accesses anymore
+  pthread_mutex_unlock(&thread_pool->m);
 
   if (status != NOTHREAD)
     mtxprintf(pt_debug, "thread created\n");
   return (status != NOTHREAD);
 }
 
-void thread_pool_shutdown(thread_pool_t *thread_pool) {
+void thread_pool_shutdown(thread_pool_t *thread_pool)
+{
   thread_pool->shutdown = 1;
 }
 
 /*
   Decrease thread pool size and broadcast update.
 */
-void pool_thread_terminate(thread_pool_t *thread_pool) {
+void pool_thread_terminate(thread_pool_t *thread_pool)
+{
   /*
     Protect against concurrent accesses. Broadcast the update.
   */
@@ -90,7 +108,8 @@ void pool_thread_terminate(thread_pool_t *thread_pool) {
 /*
   Wait for pool size to be empty.
 */
-void wait_thread_pool_empty(thread_pool_t *thread_pool) {
+void wait_thread_pool_empty(thread_pool_t *thread_pool)
+{
   /*
     Wait for thread pool size to be equal zero. Protect section against
     concurrent access
@@ -99,6 +118,7 @@ void wait_thread_pool_empty(thread_pool_t *thread_pool) {
     mtxprintf(pt_debug, "pool not empty, exit process\n");
 }
 
-int get_shutdown(thread_pool_t *thread_pool) {
-  return thread_pool->shutdown; 
+int get_shutdown(thread_pool_t *thread_pool)
+{
+  return thread_pool->shutdown;
 }
