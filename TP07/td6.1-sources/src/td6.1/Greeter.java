fac594917d2d11847079d0ee296ffb640b53d225
Alaf Do Nascimento Santos <asantos-22@tp-4b01-04.enst.fr>
2023-04-03 11:24:32 +0200

    Simple Hello World

---
+++ TP07/td6.1-sources/src/td6.1/Greeter.java
@@ -0,0 +1,37 @@
+// Simple Hello-World program based on Akka actors.
+//
+// Based on the code from Akka's tutorial/example suite:
+// http://doc.akka.io/docs/akka/2.3.14/java/hello-world.html
+//
+
+package se205.td6_1;
+
+import akka.actor.UntypedActor;
+
+public class Greeter extends UntypedActor {
+
+  // Define messages that are sent/received by the Greeter
+  public static enum Msg {
+    GREET, DONE;
+  }
+
+  @Override
+  public void onReceive(Object msg) {
+    // Check the message
+    if (msg == Msg.GREET) {
+      // handle all GREET messages ...
+      System.out.println("Hello World!");
+
+      String strOut = "Path of the sending actor: " + getSender().path();
+      strOut += "\n" + "Path of the Greeter actor: " + getSelf().path();
+      System.out.println(strOut);
+
+      // then send a reply message.
+      getSender().tell(Msg.DONE, getSelf());
+    } else {
+      // signal that an unexpected message was received
+      unhandled(msg);
+    }
+  }
+
+}
